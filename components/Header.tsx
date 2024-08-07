02c77e7505ea657698ddb506d75dcb2d69152084
Quincy Fer <quincy.e.fer@gmail.com>
2024-08-07 15:36:31 +0200

    Add useEffect and state => if scrolling reduce nav size

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,20 +1,49 @@
+"use client";
+
 import Link from "next/link";
 import MobileNav from "./MobileNav";
 import Nav from "./Nav";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 const Header = () => {
+  const [headerActive, setHeaderActive] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      // detect scroll
+      setHeaderActive(window.scrollY > 50);
+    };
+
+    //  add scroll event
+    window.addEventListener("scroll", handleScroll);
+    //  clear scroll event
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <header className="fixed left-0 right-0 w-full bg-primary-200 h-[90px]">
-      <div className="container mx-auto">
+    <header
+      className={`${
+        headerActive ? "h-[100px]" : "h-[124px]"
+      } fixed left-0 right-0 w-full bg-primary-200 h-[100px] transition-all z-50`}
+    >
+      <div className="container mx-auto h-full flex items-center justify-between">
         {/* Logo */}
         <Link href="/">
-          <Image src={"/images/logo.png"} alt="logo" width={117} height={55} />
+          <Image
+            src={"/images/logo.png"}
+            alt="logo"
+            width={117}
+            height={55}
+            priority={true}
+          />
         </Link>
         {/* mobile nav hidden on desktop */}
-        <MobileNav containerStyles="xl:hidden" />
+        <MobileNav containerStyles="xl:hidden text-white" />
         {/* desktop nav hidden on mobile */}
-        <Nav containerStyles="flex xl:flex gap-4 hidden" />
+        <Nav containerStyles="flex xl:flex gap-4 hidden text-white" />
       </div>
     </header>
   );

2b5871157ef39c7f234d3720ff4522666f38832b
Quincy Fer <quincy.e.fer@gmail.com>
2024-08-07 14:48:56 +0200

    Create Working Links to each section, fixed nav

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,13 @@
+import Nav from "./Nav";
+
 const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header className="fixed left-0 right-0 w-full bg-white">
+      <div className="container mx-auto">
+        <Nav containerStyles="bg-red-300 py-12 flex gap-4" />
+      </div>
+    </header>
+  );
 };
 
 export default Header;

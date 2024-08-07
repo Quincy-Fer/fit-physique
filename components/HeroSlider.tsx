4d8bc31c2b97a75dca77e298a1b55d24c20c0ed4
Quincy Fer <quincy.e.fer@gmail.com>
2024-08-07 20:03:37 +0200

    Rearrange layout, create second slide

--- components/HeroSlider.tsx
+++ components/HeroSlider.tsx
@@ -11,19 +11,38 @@ import CustomButton from "./CustomButton";
 const Heroslider = () => {
   return (
     <Swiper>
-      <SwiperSlide className="h-full flex justify-end pt-48">
-        <div>
-          <h1 className="h1 text-center lg:text-left mb-2">
-            <span>Where hard</span> work meets succes
-          </h1>
-          <p className="text-white italic text-center lg:text-left mb-4 ">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Non quo
-            commodi doloribus.
-          </p>
-          <CustomButton
-            text="Get Started"
-            containerStyles="w-[196px] h-[62px] "
-          />
+      <SwiperSlide>
+        <div className="h-full flex justify-end pt-48">
+          <div className="flex flex-col items-center lg:items-start lg:max-w-[700px]">
+            <h1 className="h1 text-center lg:text-left mb-2">
+              <span>Where hard</span> work meets succes
+            </h1>
+            <p className="text-white italic text-center lg:text-left mb-4 ">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Non quo
+              commodi doloribus.
+            </p>
+            <CustomButton
+              text="Get Started"
+              containerStyles="w-[196px] h-[62px] "
+            />
+          </div>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="h-full flex justify-end pt-48">
+          <div className="flex flex-col items-center lg:items-start lg:max-w-[700px]">
+            <h1 className="h1 text-center lg:text-left mb-2">
+              <span>Where hard</span> work meets succes
+            </h1>
+            <p className="text-white italic text-center lg:text-left mb-4 ">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Non quo
+              commodi doloribus.
+            </p>
+            <CustomButton
+              text="Get Started"
+              containerStyles="w-[196px] h-[62px] "
+            />
+          </div>
         </div>
       </SwiperSlide>
     </Swiper>

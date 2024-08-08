7eb7f289be54f72e8760a4a7fb7fab749e197f96
Quincy Fer <quincy.e.fer@gmail.com>
2024-08-08 16:16:59 +0200

    Add animation

--- components/Classes.tsx
+++ components/Classes.tsx
@@ -11,35 +11,41 @@ const classes = [
     img: "/images/bodybuilding.jpg",
     description:
       "Lorem ipsum, dolor sit amet consectetur adipisicing elit.\
-     Vitae eaque sapiente expedita sit laudantium officiis!",
+     Vitae eaque sapiente expedita sit.",
   },
   {
     name: "cardio",
     img: "/images/cardio.jpg",
     description:
       "Lorem ipsum, dolor sit amet consectetur adipisicing elit.\
-     Vitae eaque sapiente expedita sit laudantium officiis!",
+     Vitae eaque sapiente expedita sit.",
   },
   {
     name: "fitness",
     img: "/images/fitness.jpg",
     description:
       "Lorem ipsum, dolor sit amet consectetur adipisicing elit.\
-     Vitae eaque sapiente expedita sit laudantium officiis!",
+     Vitae eaque sapiente expedita sit.",
   },
   {
     name: "crossfit",
     img: "/images/crossfit.jpg",
     description:
       "Lorem ipsum, dolor sit amet consectetur adipisicing elit.\
-     Vitae eaque sapiente expedita sit laudantium officiis!",
+     Vitae eaque sapiente expedita sit.",
   },
 ];
 
 const Classes = () => {
   return (
     <section id="class">
-      <div className="grid grid-cols-1 lg:grid-cols-2">
+      <motion.div
+        variants={fadeIn("up", 0.6)}
+        initial="hidden"
+        whileInView={"show"}
+        viewport={{ once: false, amount: 0.2 }}
+        className="grid grid-cols-1 lg:grid-cols-2"
+      >
         {classes.map((item, index) => {
           return (
             <div
@@ -52,13 +58,40 @@ const Classes = () => {
               <Image src={item.img} fill className="object-cover" alt="" />
               {/* text and button */}
               <div className="z-30 text-center w-[380px] flex flex-col items-center justify-center gap-4 ">
-                <h3 className="h3 text-accent">{item.name}</h3>
+                <motion.h3
+                  variants={fadeIn("up", 0.4)}
+                  initial="hidden"
+                  whileInView={"show"}
+                  viewport={{ once: false, amount: 0.2 }}
+                  className="h3 text-accent"
+                >
+                  {item.name}
+                </motion.h3>
+                <motion.p
+                  variants={fadeIn("up", 0.6)}
+                  initial="hidden"
+                  whileInView={"show"}
+                  viewport={{ once: false, amount: 0.2 }}
+                  className="text-white"
+                >
+                  {item.description}
+                </motion.p>
+                <motion.div
+                  variants={fadeIn("up", 0.8)}
+                  initial="hidden"
+                  whileInView={"show"}
+                  viewport={{ once: false, amount: 0.2 }}
+                >
+                  <CustomButton
+                    containerStyles="w-[164px] h-[46px]"
+                    text="Read More"
+                  />
+                </motion.div>
               </div>
-             
             </div>
           );
         })}
-      </div>
+      </motion.div>
     </section>
   );
 };

46dd332bc10b3b5fa1d115f81d3f5dc8837ac42b
Quincy Fer <quincy.e.fer@gmail.com>
2024-08-08 13:10:02 +0200

    Create about section, style h2 header 3 divs with icon h4 and p

--- components/About.tsx
+++ components/About.tsx
@@ -24,7 +24,7 @@ const featured = [
     icon: <FaDumbbell />,
     title: "modern equipment",
     subtitle:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa recusandae aut beatae cumque mollitia vero suscipit.",
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa recusandae aut beatae cumque mollitia vero suscipit. ",
   },
 ];
 
@@ -32,13 +32,37 @@ const About = () => {
   return (
     <section className="pt-8 pb-14 lg:pt-16 lg:pb-28" id="about">
       <div className="container mx-auto">
-        <div>
-          <h2>About Us</h2>
-          <p>
+        <div className="flex flex-col items-center gap-2 mb-8">
+          <h2 className="h2 text-center">About Us</h2>
+          <p className="max-w-[600px] mx-auto text-center">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
             facere, dicta fugiat voluptatem tempore illum.
           </p>
         </div>
+        {/* featured items */}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mb-16">
+          {featured.map((feature, index) => {
+            return (
+              <div
+                key={index}
+                className="flex flex-col justify-center items-center gap-4 border p-10"
+              >
+                <div
+                  className="text-4xl bg-primary-300 text-white w-[80px] h-[80px] 
+                rounded-full flex justify-center items-center"
+                >
+                  {feature.icon}
+                </div>
+                <div className="flex flex-col justify-center items-center gap-2 text-center">
+                  <h4 className="h4 text-accent"> {feature.title} </h4>
+                  <p>{feature.subtitle}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        {/* achievements */}
+        <Achievements />
       </div>
     </section>
   );

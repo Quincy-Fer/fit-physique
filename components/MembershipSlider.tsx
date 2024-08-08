2b36855608da4de5fbefc941402bdc37f667d60d
Quincy Fer <quincy.e.fer@gmail.com>
2024-08-08 18:31:05 +0200

    Enter data for picing tables

--- components/MembershipSlider.tsx
+++ components/MembershipSlider.tsx
@@ -16,8 +16,120 @@ import { Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
 
+// data
+
+const membershipData = [
+  {
+    title: "Standard",
+    price: "30",
+    benefits: [
+      {
+        icon: FaCheck,
+        text: "Includes membership",
+      },
+      {
+        icon: FaCheck,
+        text: "Acces to all gym facilities",
+      },
+      {
+        icon: MdClose,
+        text: "Diet plan included",
+      },
+      {
+        icon: FaCheck,
+        text: "Health and fitness tips",
+      },
+      {
+        icon: MdClose,
+        text: "Monday-Friday gym access",
+      },
+      {
+        icon: FaCheck,
+        text: "Full access to everything",
+      },
+      {
+        icon: MdClose,
+        text: "No additional amenities",
+      },
+    ],
+  },
+  {
+    title: "Ultimate",
+    price: "45",
+    benefits: [
+      {
+        icon: FaCheck,
+        text: "Includes membership",
+      },
+      {
+        icon: FaCheck,
+        text: "Acces to all gym facilities",
+      },
+      {
+        icon: FaCheck,
+        text: "Diet plan included",
+      },
+      {
+        icon: FaCheck,
+        text: "Health and fitness tips",
+      },
+      {
+        icon: FaCheck,
+        text: "Monday-Friday gym access",
+      },
+      {
+        icon: FaCheck,
+        text: "Full access to everything",
+      },
+      {
+        icon: MdClose,
+        text: "No additional amenities",
+      },
+    ],
+  },
+  {
+    title: "Professional",
+    price: "60",
+    benefits: [
+      {
+        icon: FaCheck,
+        text: "Includes membership",
+      },
+      {
+        icon: FaCheck,
+        text: "Acces to all gym facilities",
+      },
+      {
+        icon: FaCheck,
+        text: "Diet plan included",
+      },
+      {
+        icon: FaCheck,
+        text: "Health and fitness tips",
+      },
+      {
+        icon: FaCheck,
+        text: "Monday-Friday gym access",
+      },
+      {
+        icon: FaCheck,
+        text: "Full access to everything",
+      },
+      {
+        icon: FaCheck,
+        text: "No additional amenities",
+      },
+    ],
+  },
+];
+
 const MembershipSlider = () => {
-  return <div>MembershipSlider</div>;
+  return (
+    <Swiper>
+      <SwiperSlide>slide 1</SwiperSlide>
+      <SwiperSlide>slide 2</SwiperSlide>
+    </Swiper>
+  );
 };
 
 export default MembershipSlider;

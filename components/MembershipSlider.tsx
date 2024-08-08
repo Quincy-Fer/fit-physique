43a8b7a71c413f81b7b5b3686f16e4bd22757085
Quincy Fer <quincy.e.fer@gmail.com>
2024-08-08 18:20:13 +0200

    Import swiper components, modules and styling.

--- components/MembershipSlider.tsx
+++ components/MembershipSlider.tsx
@@ -1,9 +1,23 @@
+"use client";
 
+import Image from "next/image";
+import { MdClose } from "react-icons/md";
+import { FaCheck } from "react-icons/fa6";
+
+import CustomButton from "./CustomButton";
+
+//  import swiper components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// import required modules
+import { Pagination } from "swiper/modules";
+
+// swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
 
 const MembershipSlider = () => {
-  return (
-    <div>MembershipSlider</div>
-  )
-}
+  return <div>MembershipSlider</div>;
+};
 
-export default MembershipSlider
+export default MembershipSlider;

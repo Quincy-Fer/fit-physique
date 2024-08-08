b27f535f8029c796ab774b1c00e6c4c5eacd2c13
Quincy Fer <quincy.e.fer@gmail.com>
2024-08-08 12:54:59 +0200

    Setup data and imports for about section

--- components/About.tsx
+++ components/About.tsx
@@ -1,5 +1,47 @@
+"use client";
+
+import { FaUsers } from "react-icons/fa";
+import { IoIosPricetags } from "react-icons/io";
+import { FaDumbbell } from "react-icons/fa6";
+
+import { motion } from "framer-motion";
+import { fadeIn } from "@/lib/variants";
+
+const featured = [
+  {
+    icon: <FaUsers />,
+    title: "award winning trainers",
+    subtitle:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa recusandae aut beatae cumque mollitia vero suscipit.",
+  },
+  {
+    icon: <IoIosPricetags />,
+    title: "excellent trainers",
+    subtitle:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa recusandae aut beatae cumque mollitia vero suscipit.",
+  },
+  {
+    icon: <FaDumbbell />,
+    title: "modern equipment",
+    subtitle:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa recusandae aut beatae cumque mollitia vero suscipit.",
+  },
+];
+
 const About = () => {
-  return <section className="h-screen bg-yellow-300" id="about">Aboutt</section>;
+  return (
+    <section className="pt-8 pb-14 lg:pt-16 lg:pb-28" id="about">
+      <div className="container mx-auto">
+        <div>
+          <h2>About Us</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
+            facere, dicta fugiat voluptatem tempore illum.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;

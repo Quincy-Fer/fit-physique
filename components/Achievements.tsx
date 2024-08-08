7d61077ce7345d1157a033cce79d996488a3feaa
Quincy Fer <quincy.e.fer@gmail.com>
2024-08-08 13:19:15 +0200

    Create Achievements file, import icons, animation library and useref

---
+++ components/Achievements.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import CountUp from "react-countup";
+import {FaBriefcase, FaClock, FaTrophy} from 'react-icons/fa'
+import {ImUsers} from 'react-icons/im'
+
+import {motion, useInView} from 'framer-motion'
+import { useRef } from "react";
+
+const Achievements = () => {
+  return <div>Achievements</div>;
+};
+
+export default Achievements;

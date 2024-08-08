6fa8207d1072f1284e8c66c3567e49a5d6cf953c
Quincy Fer <quincy.e.fer@gmail.com>
2024-08-08 16:08:09 +0200

    Map over items, for each create div with image, overlay, text and button

--- components/Classes.tsx
+++ components/Classes.tsx
@@ -38,8 +38,27 @@ const classes = [
 
 const Classes = () => {
   return (
-    <section className="h-screen bg-green-300" id="class">
-      Classes
+    <section id="class">
+      <div className="grid grid-cols-1 lg:grid-cols-2">
+        {classes.map((item, index) => {
+          return (
+            <div
+              className="relative w-full h-[300px] lg:h-[485px] flex flex-col 
+              justify-center items-center"
+              key={index}
+            >
+              {/* overlay */}
+              <div className="bg-black/40 absolute w-full h-full top-0 z-10"></div>
+              <Image src={item.img} fill className="object-cover" alt="" />
+              {/* text and button */}
+              <div className="z-30 text-center w-[380px] flex flex-col items-center justify-center gap-4 ">
+                <h3 className="h3 text-accent">{item.name}</h3>
+              </div>
+             
+            </div>
+          );
+        })}
+      </div>
     </section>
   );
 };

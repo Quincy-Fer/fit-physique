0675c361c5a0250c7fac607f643f70ad6aab9a15
Quincy Fer <quincy.e.fer@gmail.com>
2024-08-09 15:57:18 +0200

    Finish Contact section of footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -21,13 +21,37 @@ const Footer = () => {
       <div className="container mx-auto pb-24">
         <div className="text-white grid grid-cols-1 xl:grid-cols-4 gap-x-8 gap-y-12">
           {/* info */}
-          <div>Info</div>
+          <div className="flex flex-col gap-4">
+            <Link href="#">
+              <Image src={"/images/logo.png"} width={117} height={55} alt="" />
+            </Link>
+            <p className="max-w-sm">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci,
+              assumenda.
+            </p>
+            <ul className="flex flex-col gap-4">
+              <li className="flex items-center gap-4">
+                <FaMapMarkerAlt className="text-xl text-accent" />
+                <span>Elysium 1, FlavorTown</span>
+              </li>
+              <li className="flex items-center gap-4">
+                <FaPhoneAlt className="text-xl text-accent" />
+                <span>123-456-789</span>
+              </li>
+              <li className="flex items-center gap-4">
+                <FaEnvelope className="text-xl text-accent" />{" "}
+                <Link href="#">
+                  <span>sayhello@gmail.com</span>
+                </Link>
+              </li>
+            </ul>
+          </div>
           {/* blog */}
-          <div>Blog</div>
+          <div className="bg-pink-300/20">Blog</div>
           {/* gallery */}
-          <div>Gallery</div>
+          <div className="bg-pink-300/20">Gallery</div>
           {/* newsletter */}
-          <div>newsletter</div>
+          <div className="bg-pink-300/20">newsletter</div>
         </div>
       </div>
     </footer>

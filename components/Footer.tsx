f12060de8c7010d27a7bd31c1892b7a101bb9223
Quincy Fer <quincy.e.fer@gmail.com>
2024-08-09 15:43:34 +0200

    Add imports, create skeleton for footer items

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,37 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+
+import {
+  FaMapMarkerAlt,
+  FaEnvelope,
+  FaPhoneAlt,
+  FaFacebook,
+  FaTwitter,
+  FaYoutube,
+} from "react-icons/fa";
+
+import CustomButton from "./CustomButton";
+import { motion } from "framer-motion";
+
 const Footer = () => {
-  return <footer className="h-[80vh] bg-orange-200">Footer</footer>;
+  return (
+    <footer className="bg-primary-300 pt-24">
+      <div className="container mx-auto pb-24">
+        <div className="text-white grid grid-cols-1 xl:grid-cols-4 gap-x-8 gap-y-12">
+          {/* info */}
+          <div>Info</div>
+          {/* blog */}
+          <div>Blog</div>
+          {/* gallery */}
+          <div>Gallery</div>
+          {/* newsletter */}
+          <div>newsletter</div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;

1c341b3d0bb7a55e172d4fd6981e4f843bea2b5e
Quincy Fer <quincy.e.fer@gmail.com>
2024-08-09 12:41:44 +0200

    Import motion add data

--- components/Testimonial.tsx
+++ components/Testimonial.tsx
@@ -13,6 +13,50 @@ import { Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
 
+import { motion } from "framer-motion";
+import { fadeIn } from "@/lib/variants";
+
+// data
+
+const testimonialData = [
+  {
+    img: "/images/lucy.jpg",
+    message:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi, sint!",
+    name: "Lucy Anthony",
+  },
+  {
+    img: "/images/michael.jpg",
+    message:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi, sint!",
+    name: "Michael Phillips",
+  },
+  {
+    img: "/images/maria.jpg",
+    message:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi, sint!",
+    name: "Maria Garcia",
+  },
+  {
+    img: "/images/lucy.jpg",
+    message:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi, sint!",
+    name: "Lucy Anthony",
+  },
+  {
+    img: "/images/michael.jpg",
+    message:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi, sint!",
+    name: "Michael Phillips",
+  },
+  {
+    img: "/images/maria.jpg",
+    message:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi, sint!",
+    name: "Maria Garcia",
+  },
+];
+
 const Testimonial = () => {
   return (
     <section className="h-[50vh] bg-indigo-300" id="testimonial">
